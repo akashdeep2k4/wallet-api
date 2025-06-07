59576ea1c0e2b18763e37c71148f5b86fb81fcbf
Akashdeep <akashdeep2k4@gmail.com>
2025-06-07 11:26:52 +0530

    commit

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  job.start();
+}
 
 app.use(rateLimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("It's working!");
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
 });
 
 app.use("/api/transactions", transactionsRoute);
